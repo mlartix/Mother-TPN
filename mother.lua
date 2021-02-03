d23fbebe21871e859c8594aa745a2195002f8176
mlartix <74879660+mlartix@users.noreply.github.com>
2021-02-02 22:00:40 -0800

    Delete mother.lua

--- mother.lua
+++
@@ -1,9 +0,0 @@
-game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-79, 115, 32)
-wait(2)
-for i = 1,20 do
-for i,v in next, workspace:GetDescendants() do 
-   if v:IsA("ProximityPrompt") then 
-       fireproximityprompt(v)
-       end
-end
-end
